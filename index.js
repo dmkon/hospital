b8c672593515da13e7dd96b0b506ad558c4c568b
dmkon <dmkon@hotmail.com>
2018-11-11 17:27:59 -0500

    Express and express-validator added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-//var express = require("express");
+var express = require("express");
 var bodyParser = require("body-parser");
+const { check, validationResult } = require('express-validator/check');
 var SERVER_NAME = 'patients-records-api'
 
 var PORT = process.env.PORT || 8000;
@@ -13,28 +14,28 @@ console.log('HOST='+HOST);
 
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
-var restify = require('restify')
-var server = restify.createServer({ name: SERVER_NAME})
+//var restify = require('restify')
+//var server = restify.createServer({ name: SERVER_NAME})
 //var server = restify.createServer({ name: SERVER_NAME})
 
-//var server = express();
+var server = express();
  //server.set('port', (process.env.PORT || 8000));
- //server.use(bodyParser.json());
+ server.use(bodyParser.json());
  //server.use(express.static(path.join(__dirname, 'public')));
 
   var url = process.env.MONGOLAB_URI_HOSPITAL;  
   console.log('MONGOLAB_URI_HOSPITAL='+url);
 
 
-  //server.set("port", PORT);
+  server.set("port", PORT);
   //server.set("host", HOST);
 
   //server.set("port", PORT);
   //server.set("host", process.env.HOST || "localhost");
 
-  server.listen(PORT, HOST, function () {
-  //var app = server.listen(server.get("port"), function() {
-  console.log('Server %s listening at %s', server.name, server.url)
+  //server.listen(PORT, HOST, function () {
+  var app = server.listen(server.get("port"), function() {
+  //console.log('Server %s listening at %s', server.name, server.url)
   //var host = app.address().address;
   //var port = app.address().port;
   console.log('Server %s listening at %s', HOST, PORT)
@@ -46,7 +47,7 @@ var server = restify.createServer({ name: SERVER_NAME})
   // console.log('http://127.0.0.1:8000/patients/:id/recordType/:type')
   // console.log('http://127.0.0.1:8000/patients/all')
   // console.log('http://127.0.0.1:8000/patients/critical')
-  if (PORT != null){
+  //if (PORT != null){
     console.log(HOST+':'+PORT+'/patients')
     console.log(HOST+':'+PORT+'/records')
     console.log(HOST+':'+PORT+'/patients/:id')
@@ -54,23 +55,23 @@ var server = restify.createServer({ name: SERVER_NAME})
     console.log(HOST+':'+PORT+'/patients/:id/recordType/:type')
     console.log(HOST+':'+PORT+'/patients/all')
     console.log(HOST+':'+PORT+'/patients/critical')
-  } else {
-    console.log(HOST+'/patients')
-    console.log(HOST+'/records')
-    console.log(HOST+'/patients/:id')
-    console.log(HOST+'/patients/:id/records')
-    console.log(HOST+'/patients/:id/recordType/:type')
-    console.log(HOST+'/patients/all')
-    console.log(HOST+'/patients/critical')
-  }
+  // } else {
+  //   console.log(HOST+'/patients')
+  //   console.log(HOST+'/records')
+  //   console.log(HOST+'/patients/:id')
+  //   console.log(HOST+'/patients/:id/records')
+  //   console.log(HOST+'/patients/:id/recordType/:type')
+  //   console.log(HOST+'/patients/all')
+  //   console.log(HOST+'/patients/critical')
+  // }
 })
 
-server
-  // Allow the use of POST
-  .use(restify.fullResponse())
+// server
+//   // Allow the use of POST
+//   .use(restify.fullResponse())
 
-  // Maps req.body to req.params so there is no switching between them
-  .use(restify.bodyParser())
+//   // Maps req.body to req.params so there is no switching between them
+//   .use(restify.bodyParser())
 
 
 //+/ GET critical
@@ -98,8 +99,8 @@ server.get('/patients/critical', function (req, res, next) {
       console.log('Finish');
       if (err2) throw err2;
       console.log(JSON.stringify(res2));
-      res.send(201, res2);
-      //res.status(201).send(res2);
+      //res.send(201, res2);
+      res.status(201).send(res2);
       db.close();
     });
   })
@@ -118,8 +119,8 @@ MongoClient.connect(url, { useNewUrlParser: true }, function(err,db){
   dbo.collection("patients").find().toArray(function(err,result){
     if(err) throw err;
     console.log(result);
-    res.send(200, result);
-    //res.status(200).send(result);
+    //es.send(200, result);
+    res.status(200).send(result);
     db.close();
   });
  })
@@ -139,8 +140,8 @@ server.get('/patients/:id', function (req, res, next) {
     dbo.collection("patients").findOne(query, function(err,result) {
       if(err) throw err;
       console.log(result);
-      res.send(200, result);
-      //res.status(200).send(result);
+      //res.send(200, result);
+      res.status(200).send(result);
       db.close();
     });
  })
@@ -161,8 +162,8 @@ server.get('patients/:id/records', function (req, res, next) {
     dbo.collection("records").find(query).toArray(function(err,result) {
       if(err) throw err;
       console.log(result);
-      res.send(result);
-      //res.status(200).send(result);
+      //res.send(result);
+      res.status(200).send(result);
       db.close();
     });
  })
@@ -188,72 +189,84 @@ server.get('/records', function (req, res, next) {
 
 //+/ POST - create a new patient ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
- server.post('/patients', function (req, res, next) {
+ server.post('/patients', 
+  [
+    // username must be an email
+    check('firstName').isLength({min:2}).withMessage('firstName must be at least 2 chars long'),
+    // password must be at least 5 chars long
+    check('lastName').isLength({min:2}).withMessage('lastName must be at least 2 chars long')
+  ],
+    function (req, res, next) {
 console.log("1");
 console.log(req.params.firstName);
-  if (req.params.firstName === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(
-      new restify.InvalidArgumentError('firstName must be supplied')
-      //server.InvalidArgumentError('firstName must be supplied')
-      )
+const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
   }
-  if (req.params.lastName === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(
-      new restify.InvalidArgumentError('lastName must be supplied')
-      //new server.InvalidArgumentError('lastName must be supplied')
-      )
-  }
-  if (req.params.dateOfBirth === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(
-      new restify.InvalidArgumentError('dateOfBirth must be supplied')
-      //new server.InvalidArgumentError('dateOfBirth must be supplied')
-      )
-  }
-  if (req.params.gender === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(
-      new restify.InvalidArgumentError('gender must be supplied')
-      //new server.InvalidArgumentError('gender must be supplied')
-      )
-  }
-  if (req.params.address === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(
-      new restify.InvalidArgumentError('address must be supplied')
-      //new server.InvalidArgumentError('address must be supplied')
-      )
-  }
-  if (req.params.city === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(
-      new restify.InvalidArgumentError('city must be supplied')
-      //new server.InvalidArgumentError('city must be supplied')
-      )
-  }
-  if (req.params.province === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(
-      new restify.InvalidArgumentError('province must be supplied')
-      //new server.InvalidArgumentError('province must be supplied')
-      )
-  }
-  if (req.params.postalCode === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(
-      new restify.InvalidArgumentError('postalCode must be supplied')
-      //new server.InvalidArgumentError('postalCode must be supplied')
-      )
-  }
-  if (req.params.doctor === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(
-      new restify.InvalidArgumentError('doctor must be supplied')
-      //new server.InvalidArgumentError('doctor must be supplied')
-      )
-  }
+
+  // if (req.params.firstName === undefined ) {
+  //   // If there are any errors, pass them to next in the correct format
+  //   return next(
+  //     new restify.InvalidArgumentError('firstName must be supplied')
+  //     //server.InvalidArgumentError('firstName must be supplied')
+  //     )
+  // }
+  // if (req.params.lastName === undefined ) {
+  //   // If there are any errors, pass them to next in the correct format
+  //   return next(
+  //     new restify.InvalidArgumentError('lastName must be supplied')
+  //     //new server.InvalidArgumentError('lastName must be supplied')
+  //     )
+  // }
+  // if (req.params.dateOfBirth === undefined ) {
+  //   // If there are any errors, pass them to next in the correct format
+  //   return next(
+  //     new restify.InvalidArgumentError('dateOfBirth must be supplied')
+  //     //new server.InvalidArgumentError('dateOfBirth must be supplied')
+  //     )
+  // }
+  // if (req.params.gender === undefined ) {
+  //   // If there are any errors, pass them to next in the correct format
+  //   return next(
+  //     new restify.InvalidArgumentError('gender must be supplied')
+  //     //new server.InvalidArgumentError('gender must be supplied')
+  //     )
+  // }
+  // if (req.params.address === undefined ) {
+  //   // If there are any errors, pass them to next in the correct format
+  //   return next(
+  //     new restify.InvalidArgumentError('address must be supplied')
+  //     //new server.InvalidArgumentError('address must be supplied')
+  //     )
+  // }
+  // if (req.params.city === undefined ) {
+  //   // If there are any errors, pass them to next in the correct format
+  //   return next(
+  //     new restify.InvalidArgumentError('city must be supplied')
+  //     //new server.InvalidArgumentError('city must be supplied')
+  //     )
+  // }
+  // if (req.params.province === undefined ) {
+  //   // If there are any errors, pass them to next in the correct format
+  //   return next(
+  //     new restify.InvalidArgumentError('province must be supplied')
+  //     //new server.InvalidArgumentError('province must be supplied')
+  //     )
+  // }
+  // if (req.params.postalCode === undefined ) {
+  //   // If there are any errors, pass them to next in the correct format
+  //   return next(
+  //     new restify.InvalidArgumentError('postalCode must be supplied')
+  //     //new server.InvalidArgumentError('postalCode must be supplied')
+  //     )
+  // }
+  // if (req.params.doctor === undefined ) {
+  //   // If there are any errors, pass them to next in the correct format
+  //   return next(
+  //     new restify.InvalidArgumentError('doctor must be supplied')
+  //     //new server.InvalidArgumentError('doctor must be supplied')
+  //     )
+  // }
   var newPatient = {
     //patient_id: req.params.patient_id,
     firstName: req.params.firstName, 
@@ -273,8 +286,8 @@ console.log(req.params.firstName);
     dbo.collection("patients").insertOne(newPatient, function(err, res2) {
       if (err) throw err;
       console.log(newPatientJSON);
-        res.send(201, newPatient);
-        //res.status(201).send(newPatient);
+        //res.send(201, newPatient);
+        res.status(201).send(newPatient);
         db.close();
     });
    })
@@ -355,8 +368,8 @@ server.post('/patients/:id/recordType/:recordType', function (req, res, next) {
       dbo.collection("records").findOne(query, function(err,result) {
         if(err) throw err;
         console.log(result);
-        res.send(201, result);
-        //res.status(201).send(result);
+        //res.send(201, result);
+        res.status(201).send(result);
         db.close();
       });
     });
@@ -365,7 +378,7 @@ server.post('/patients/:id/recordType/:recordType', function (req, res, next) {
 
 //+/ Delete all patients and their records //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-server.del('/patients/all', function (req, res, next) {
+server.delete('/patients/all', function (req, res, next) {
   console.log("patients/all");
     MongoClient.connect(url, { useNewUrlParser: true }, function(err,db){
       if(err) throw err;
@@ -378,15 +391,15 @@ server.del('/patients/all', function (req, res, next) {
           if (err) throw err;
           console.log("records deleted");
           res.send(201, "deleted")
-          //res.status(201).send("deleted");
-          db.close();
+          res.status(201).send("deleted");
+          //db.close();
         });
       });
      })
   })
 //+/ Delete patient's records by patient id and record type//////////////////////////////////////////////////////////////////////////////////////
 // Delete all Blood Pressure or all Respiratory Rate ...
-server.del('/patients/:id/recordType/:recordType', function (req, res, next) {
+server.delete('/patients/:id/recordType/:recordType', function (req, res, next) {
   MongoClient.connect(url, { useNewUrlParser: true }, function(err,db){
     if(err) throw err;
     var dbo = db.db("hospital");
@@ -400,8 +413,8 @@ server.del('/patients/:id/recordType/:recordType', function (req, res, next) {
     dbo.collection("records").deleteMany(query, function(err, res2) {
       if (err) throw err;
       console.log("record deleted");
-      res.send(201, "deleted")
-      //res.status(201).send("deleted");
+      //res.send(201, "deleted")
+      res.status(201).send("deleted");
       db.close();
     });
    })
@@ -410,7 +423,7 @@ server.del('/patients/:id/recordType/:recordType', function (req, res, next) {
 
 // Delete a single patient by patient id //////////////////////////////////////////////////////////////////////////////////////////////////
 
-server.del('/patients/:id', function (req, res, next) {
+server.delete('/patients/:id', function (req, res, next) {
   MongoClient.connect(url, { useNewUrlParser: true }, function(err,db){
     if(err) throw err;
     var dbo = db.db("hospital");
@@ -421,8 +434,8 @@ server.del('/patients/:id', function (req, res, next) {
     dbo.collection("patients").deleteOne(query, function(err, res2) {
       if (err) throw err;
       console.log("patient deleted");
-      res.send(201, "patient deleted")
-      //res.status(201).send("patient deleted");
+      //res.send(201, "patient deleted")
+      res.status(201).send("patient deleted");
       db.close();
     });
    })
@@ -430,7 +443,7 @@ server.del('/patients/:id', function (req, res, next) {
 
 //+/ Delete patient's records by patient id /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-server.del('/patients/:id/records', function (req, res, next) {
+server.delete('/patients/:id/records', function (req, res, next) {
   console.log("START DELETE");
   MongoClient.connect(url, { useNewUrlParser: true }, function(err,db){
     if(err) throw err;
@@ -443,8 +456,8 @@ server.del('/patients/:id/records', function (req, res, next) {
     dbo.collection("records").deleteMany(query, function(err, res2) {
       if (err) throw err;
       console.log("records deleted");
-      res.send(201, "deleted")
-      //res.status(201).send("deleted");
+      //res.send(201, "deleted")
+      res.status(201).send("deleted");
       db.close();
     });
    })
@@ -452,7 +465,7 @@ server.del('/patients/:id/records', function (req, res, next) {
 
 //+/ Delete single record by record id /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-server.del('/records/:id', function (req, res, next) {
+server.delete('/records/:id', function (req, res, next) {
   console.log("START DELETE");
   MongoClient.connect(url, { useNewUrlParser: true }, function(err,db){
     if(err) throw err;
@@ -465,8 +478,8 @@ server.del('/records/:id', function (req, res, next) {
     dbo.collection("records").deleteOne(query, function(err, res2) {
       if (err) throw err;
       console.log("record deleted");
-      res.send(201, "deleted")
-      //res.status(201).send("deleted");
+      //res.send(201, "deleted")
+      res.status(201).send("deleted");
       db.close();
     });
    })
@@ -563,8 +576,8 @@ MongoClient.connect(url, { useNewUrlParser: true }, function(err,db){
     if (err) throw err;
     console.log("patient updated");
     console.log(newPatient);
-    res.send(201, newPatient);
-    //res.status(201).send(newPatient);
+    //res.send(201, newPatient);
+    res.status(201).send(newPatient);
     db.close();
   });
  })
@@ -629,8 +642,8 @@ console.log("record update");
       if (err) throw err;
       console.log("record updated");
       console.log(newRecord);
-      res.send(201, newRecord);
-      //res.status(201).send(newRecord);
+      //res.send(201, newRecord);
+      res.status(201).send(newRecord);
       db.close();
     });
    })
